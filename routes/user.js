36e3d71a7fceae182a01b697908ca3b4313b8de2
Guilherme Silva <sotrabalhosgui@hotmail.com>
2020-12-11 21:09:10 +0000

    Login e Registo implementados

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 let express = require("express");
+let bcrypt = require("bcryptjs");
 let router = express.Router();
 let dbConnection = require("../config/db_conn");
-let userSchema = require("../schemas/user.schema");
+let userSchemas = require("../schemas/user.schema");
+//TODO: Criar middleware que verifica a cookie de sessão
 
 // GET /
 router.get("/", (req, res, next) => {
@@ -9,7 +11,54 @@ router.get("/", (req, res, next) => {
 });
 
 // POST /
-router.post("/", (req, res, next) => {});
+router.post("/", async (req, res, next) => {
+  //Validação de dados do frontend
+  let validatedUser = userSchemas.userLoginSchema.validate(req.body);
+
+  if (validatedUser.error) {
+    res.status(400).json({
+      message: validatedUser.error.details[0].context.label,
+    });
+    return;
+  }
+
+  validatedUser = validatedUser.value;
+
+  //Verificar se a conta existe na base de dados, se sim, buscar o id do user e criar uma cookie de sessão no cliente com essa cookie encriptada
+  try {
+    let rows = await (
+      await dbConnection
+    ).query("SELECT user_id FROM User WHERE email=? AND password=?;", [
+      validatedUser.email,
+      validatedUser.password,
+    ]);
+    //TODO: atualizar para funcionar com hash da palavra passe (se calhar primeiro verificar email, se existir buscar a palavra passe desse email e fazer compare com o bcrypt com a inserida)
+
+    //Caso não haja conta
+    if (!rows || rows.length === 0) {
+      console.log("[ERRO] Conta não existe");
+      res.status(400).json({
+        message:
+          "Essa conta não existe, verifique se os dados estão bem inseridos",
+      });
+      return;
+    }
+
+    //TODO: Caso tudo esteja correto, criar cookie de sessão e redirecionar para dashboard
+    // req.session.sessionCookie = rows[0].user_id; //Pseudocódigo
+    console.log("[SUCESSO] Conta existe");
+    res.status(200).json({
+      message: "Conta existe, criar cookie de sessão e redirecionar",
+    });
+  } catch (err) {
+    console.log(
+      "[ERRO] Ocorreu algum erro a efetuar log-in\n" + err.stack || err.message
+    );
+    return res
+      .status(500)
+      .json({ message: "Ocorreu algum erro, tente novamente mais tarde" });
+  }
+});
 
 // GET /registo
 router.get("/registo", (req, res, next) => {
@@ -18,7 +67,10 @@ router.get("/registo", (req, res, next) => {
 
 // POST /registo
 router.post("/registo", async (req, res, next) => {
-  let validatedUser = userSchema.validate(req.body);
+  //Encriptação da palavra passe
+  req.body.password = bcrypt.hashSync(req.body.password, 14);
+  //Validação de dados do frontend
+  let validatedUser = userSchemas.userRegisterSchema.validate(req.body);
 
   if (validatedUser.error) {
     res.status(400).json({
@@ -53,6 +105,10 @@ router.post("/registo", async (req, res, next) => {
 
     if (dbResponse.affectedRows === 1) {
       return res.status(201).json({ message: "Conta criada com sucesso" });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Ocorreu algum erro, tente novamente mais tarde" });
     }
   } catch (err) {
     console.log(
--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,16 +1,27 @@
 let Joi = require("joi");
 
-userSchema = Joi.object({
+//Schema para registo de uma conta
+userRegisterSchema = Joi.object({
   nome: Joi.string().max(256).required().label("Nome inserido incorretamente"),
   email: Joi.string()
     .email()
     .max(320)
     .required()
     .label("Email inserido incorretamente"),
-  password: Joi.string()
-    .max(256)
-    .required()
-    .label("Password inserida incorretamente"),
+  password: Joi.string().max(256).label("Password inserida incorretamente"),
 });
 
-module.exports = userSchema;
+//Schema para login de um user
+userLoginSchema = Joi.object({
+  email: Joi.string()
+    .email()
+    .max(320)
+    .required()
+    .label("Email inserido incorretamente"),
+  password: Joi.string().max(256).label("Password inserida incorretamente"),
+});
+
+module.exports = {
+  userRegisterSchema: userRegisterSchema,
+  userLoginSchema: userLoginSchema,
+};
