a1e88e0551e34653a8c1e2bc187fcde886150112
Guilherme Silva <sotrabalhosgui@hotmail.com>
2020-12-20 14:35:30 +0000

    Atualização dinamica de categorias

--- public/js/categorias.js
+++ public/js/categorias.js
@@ -1,3 +1,31 @@
+async function carregarCategorias() {
+  let div_container_categorias = document.querySelector(".container-items");
+  div_container_categorias.innerHTML = "";
+
+  let res = await fetch("/buscarCategorias");
+
+  if (res.ok) {
+    res = await res.json();
+    for (categoria of res) {
+      let newCategoria = document.createElement("div");
+      newCategoria.className = "item";
+      newCategoria.style = "background-color:" + categoria.cor + ";";
+      newCategoria.id = categoria.categoria_id;
+
+      let newParagrafo = document.createElement("p");
+      newParagrafo.className = "titulo-categoria";
+      newParagrafo.innerText = categoria.titulo;
+
+      newCategoria.appendChild(newParagrafo);
+      div_container_categorias.appendChild(newCategoria);
+    }
+  } else {
+    alert("Ocorreu algum erro");
+  }
+}
+
+carregarCategorias();
+
 document
   .querySelector(".categoriaSubmit")
   .addEventListener("click", async (e) => {
@@ -17,6 +45,7 @@ document
 
     switch (response.status) {
       case 201:
+        carregarCategorias();
         //Atualizar div com as categorias, se calhar em vez de passar as categorias pelo PUG, alterar para formato API para poder atualizar o div sem ter de re entrar na página
         alert(msg);
         break;
