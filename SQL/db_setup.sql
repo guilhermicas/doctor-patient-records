2725d0a7d08e43319b87f68b858986c20997804b
Guilherme Silva <sotrabalhosgui@hotmail.com>
2021-01-09 18:46:18 +0000

    Atualizacao do modelo de dados

--- SQL/db_setup.sql
+++ SQL/db_setup.sql
@@ -17,7 +17,7 @@ CREATE TABLE Categoria (
   categoria_id INT AUTO_INCREMENT,
   user_id INT NOT NULL,
   titulo VARCHAR(120) NOT NULL,
-  descricao VARCHAR(256),
+  descricao VARCHAR(256), 
   cor VARCHAR(7) NOT NULL DEFAULT "#F00",
   PRIMARY KEY (categoria_id),
   FOREIGN KEY (user_id) REFERENCES User(user_id)
@@ -28,17 +28,9 @@ CREATE TABLE Paciente (
   user_id INT NOT NULL,
   categoria_id INT,
   nome VARCHAR(256) NOT NULL,
+  descricao VARCHAR(1024),
   PRIMARY KEY (paciente_id),
   created_at DATE DEFAULT CURRENT_TIME,
   FOREIGN KEY (user_id) REFERENCES User(user_id),
   FOREIGN KEY (categoria_id) REFERENCES Categoria(categoria_id)
 );
-
---CREATE TABLE Atributo (
-  --atributo_id INT AUTO_INCREMENT,
-  --paciente_id INT NOT NULL,
-  --titulo VARCHAR(120) NOT NULL,
-  --descricao VARCHAR(1000),
-  --PRIMARY KEY (atributo_id),
-  --FOREIGN KEY (paciente_id) REFERENCES Paciente(paciente_id)
---);
