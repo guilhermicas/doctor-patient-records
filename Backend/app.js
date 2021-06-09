431f5e7f4a8e592232bd4b97bf447c4625d5ba10
guilhermicas <sotrabalhosgui@hotmail.com>
2021-06-09 19:30:59 +0100

    Correçao de CORS

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let app = express();
+let cors = require("cors");
 require("dotenv").config();
 
 // Configuracao do servidor
@@ -8,6 +9,16 @@ const PORT = process.env.PORT || 3000;
 // Handling de request
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Configuração CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Routes
 userRoutes = require("./routes/user");
