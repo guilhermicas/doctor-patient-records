cb288ae9549693fe82d2a6d7f035b06e1b866ca9
guilhermicas <sotrabalhosgui@hotmail.com>
2021-06-09 19:34:30 +0100

    Implementacao de login e pagina de login

--- Frontend/frontend/src/components/Landing/Landing.jsx
+++ Frontend/frontend/src/components/Landing/Landing.jsx
@@ -1,11 +1,124 @@
-import React from 'react'
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
 
-const Landing = () => {
-    return (
-        <div>
-            <h1>Landing page</h1>
+import Copyright from '../Copyright/Copyright'
+
+import useStyles from './styles'
+
+export default function Landing({checkIsAuth}) {
+  let formValues = {
+      "email":"",
+      "password":""
+  }
+
+  const classes = useStyles();
+
+  async function handleSubmit(evt){
+    evt.preventDefault()
+
+    let fetchOpts = {
+        method: "POST",
+        body: JSON.stringify(formValues),
+          
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }
+
+    console.log(formValues)
+    //Tentativa de login
+    let res = await fetch("http://localhost:2262/",fetchOpts)
+    switch (res.status) {
+        case 200:
+            alert("login efetuado com sucesso")
+            break;
+    
+        default:
+            let jsonRes = await res.json()
+            alert(jsonRes.message)
+            break;
+    }
+  }
+
+  function handleInputChange(evt) {
+    const target = evt.target;
+
+    //Passar os valores dos inputs para um objeto para depois ser enviado num POST para login
+    const value = target.value;
+    const name = target.name;
+
+    formValues[name] = value
+  }
+
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={(evt) => handleSubmit(evt)} method="post">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(evt) => handleInputChange(evt)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(evt) => handleInputChange(evt)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Log In
+            </Button>
+            <Grid container justify="center">
+              <Grid item>
+                <Link href="/registo" variant="body2">
+                  {"Não tem uma conta? Clique aqui para se registar"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
         </div>
-    )
+      </Grid>
+    </Grid>
+  );
 }
-
-export default Landing
